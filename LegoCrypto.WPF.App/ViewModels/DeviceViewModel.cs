44045aa5a64ee348a6672fe63a6e8342626ba070
drake-vcu <dkaempfer46@gmail.com>
2018-11-18 07:00:02 -0600

    add IO wpf Views

---
+++ LegoCrypto.WPF.App/ViewModels/DeviceViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LegoCrypto.IO;
+
+namespace LegoCrypto.WPF.App
+{
+    public class DeviceViewModel : ViewModelBase
+    {
+        private List<COMPortInfo> _comPorts;
+        public List<COMPortInfo> ComPorts { get => _comPorts; set => SetProperty(ref _comPorts, value); }
+
+        public string DisplayMember { get; set; } = "Description";
+
+        public RelayCommand<object> SelectionChangedCmd { get; set; }
+
+        public DeviceViewModel()
+        {
+            SelectionChangedCmd = new RelayCommand<object>(SelectionChanged);
+            ComPorts = COMPortInfo.GetCOMPortsInfo();
+        }
+
+        public void SelectionChanged(object sender)
+        {
+
+        }
+    }
+}
