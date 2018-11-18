383e1192c9614862ec33eae82333fc3b29c7f403
drake-vcu <dkaempfer46@gmail.com>
2018-11-17 21:02:16 -0600

    add tokens tab to App

---
+++ LegoCrypto.WPF.App/ViewModels/TokenSelectionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using LegoCrypto.Data;
+using LegoCrypto.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoCrypto.WPF.App
+{
+    public class TokenSelectionViewModel : ViewModelBase
+    {
+        private readonly string _charactermap = $"Resources{Path.DirectorySeparatorChar}charactermap.json";
+        private Character[] _characters;
+        public Character[] Characters { get => _characters; set => SetProperty(ref _characters, value); }
+
+        private readonly string _vehiclemap = $"Resources{Path.DirectorySeparatorChar}vehiclemap.json";
+        private Vehicle[] _vehicles;
+        public Vehicle[] Vehicles { get => _vehicles; set => SetProperty(ref _vehicles, value); }
+
+        public TokenSelectionViewModel()
+        {
+            Characters = TokenRepo<Character>.Load(_charactermap);
+            Vehicles = TokenRepo<Vehicle>.Load(_vehiclemap);
+        }
+    }
+}
