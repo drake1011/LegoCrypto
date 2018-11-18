42cdba198d7d3eaa4985442030adc0e4de90a760
drake-vcu <dkaempfer46@gmail.com>
2018-11-17 23:24:55 -0600

    added error catching for data file load

--- LegoCrypto.WPF.App/ViewModels/TokenSelectionViewModel.cs
+++ LegoCrypto.WPF.App/ViewModels/TokenSelectionViewModel.cs
@@ -6,23 +6,48 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace LegoCrypto.WPF.App
 {
     public class TokenSelectionViewModel : ViewModelBase
     {
-        private readonly string _charactermap = $"Resources{Path.DirectorySeparatorChar}charactermap.json";
+        private readonly string _charactermap = "charactermap.json";
+        private readonly string _vehiclemap = "vehiclemap.json";
+
         private Character[] _characters;
         public Character[] Characters { get => _characters; set => SetProperty(ref _characters, value); }
 
-        private readonly string _vehiclemap = $"Resources{Path.DirectorySeparatorChar}vehiclemap.json";
         private Vehicle[] _vehicles;
         public Vehicle[] Vehicles { get => _vehicles; set => SetProperty(ref _vehicles, value); }
 
+        private string _statusMessage = string.Empty;
+        public string StatusMessage { get => _statusMessage; set => SetProperty(ref _statusMessage, value); }
+
         public TokenSelectionViewModel()
         {
-            Characters = TokenRepo<Character>.Load(_charactermap);
-            Vehicles = TokenRepo<Vehicle>.Load(_vehiclemap);
+            StatusMessage += " | ";
+            try
+            {
+                Characters = TokenRepo<Character>.Load($"Resources{Path.DirectorySeparatorChar}{_charactermap}");
+                StatusMessage += $"{Characters.Length} Characters";
+            }
+            catch
+            {
+                StatusMessage += $"Error loading {_charactermap}";
+            }
+
+            StatusMessage += " | ";
+
+            try
+            { 
+                Vehicles = TokenRepo<Vehicle>.Load($"Resources{Path.DirectorySeparatorChar}{_vehiclemap}");
+                StatusMessage += $"{Vehicles.Length} Vehicles";
+            }
+            catch
+            {
+                StatusMessage += $"Error loading {_vehiclemap}";
+            }
         }
     }
 }
