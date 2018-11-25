ae98c146dbb7184fca44b68145f6da14acf5b05c
drake-vcu <dkaempfer46@gmail.com>
2018-11-25 01:47:47 -0600

    add api

---
+++ LegoCrypto.API/Controllers/CryptoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LegoCrypto.Data.Model;
+using System.Runtime.Serialization;
+
+namespace LegoCrypto.API.Controllers
+{
+    [Route("api/[controller]/encrypt")]
+    [ApiController]
+    public class CryptoController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<ITag> Encrypt(TagRequestId request)
+        {
+            try
+            {
+                ITag tag;
+                tag = TagFactory.CreateTag(request.id, request.uid);
+                tag.Encrypt();
+                return new ActionResult<ITag>(tag);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ LegoCrypto.Data/Model/TagRequestId.cs
@@ -0,0 +1,8 @@
+﻿namespace LegoCrypto.Data.Model
+{
+    public class TagRequestId
+    {
+        public uint id { get; set; }
+        public string uid { get; set; }
+    }
+}
